99fa911f201f27fac7054c40132ef76bca011e04
hansajaLisan625 <hansaja6.icmc@gmail.com>
2024-10-05 11:34:07 +0530

    Header and Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
-	return (
-		<div>App<  /div>
-	);
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
 };
 export default App;
