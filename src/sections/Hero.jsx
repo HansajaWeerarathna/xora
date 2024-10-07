ffdb9a9d2950461bfd289ed42443ed7ae2288422
hansajaLisan625 <hansaja6.icmc@gmail.com>
2024-10-07 14:14:02 +0530

    Footer section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,9 @@
-import { Element } from "react-scroll";
-import { Link as LinkScroll } from "react-scroll";
+import { Element, Link as LinkScroll } from "react-scroll";
 import Button from "../components/Button.jsx";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-60 max-lg:pt-52 max-lg:p-36 max-md:pt-36 max-md:pb-32 ">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
@@ -12,7 +11,7 @@ const Hero = () => {
               Video Editing
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly Simple
+              Amazingly simple
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
               We designed XORA AI Video Editor to be an easy to use, quick to
@@ -22,6 +21,7 @@ const Hero = () => {
               <Button icon="/images/zap.svg">Try it now</Button>
             </LinkScroll>
           </div>
+
           <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
             <img
               src="/images/hero.png"
@@ -34,4 +34,5 @@ const Hero = () => {
     </section>
   );
 };
+
 export default Hero;
